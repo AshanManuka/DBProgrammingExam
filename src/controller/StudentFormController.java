f51f94f4ee35ecddb8996e4d695510552e043352
Manuka <manukajayarathne.coma@gmail.com>
2022-07-12 10:08:15 +0530

    implemented student ui

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class StudentFormController {
+    public AnchorPane context;
+    public JFXTextField studentId;
+    public JFXTextField studentName;
+    public JFXTextField studentMail;
+    public JFXTextField studentContact;
+    public JFXTextField studentAddress;
+    public JFXTextField studentNIC;
+    public JFXButton addBtn;
+    public JFXButton updateBtn;
+    public JFXButton deleteBtn;
+    public JFXTextField searchStudentId;
+    public JFXButton searchBtn;
+
+    public void addBtn(ActionEvent actionEvent) {
+    }
+
+    public void updateBtn(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtn(ActionEvent actionEvent) {
+    }
+
+    public void searchBtn(ActionEvent actionEvent) {
+    }
+}
