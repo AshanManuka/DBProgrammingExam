f5c34667bd4e0350d45452a0d078e70731c23bef
Manuka <manukajayarathne.coma@gmail.com>
2022-07-12 09:44:13 +0530

    recorrect databaseConnection

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/imsacademy", "root", "1234");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ijseRegistation", "root", "1234");
     }
     public static DBConnection getInstance() throws  SQLException,ClassNotFoundException{
         return (dbConnection == null)? dbConnection = new DBConnection(): dbConnection;
